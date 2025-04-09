5bfe5c3baf658d2834918a5a533d52d36bf7c40b
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2025-04-09 21:38:13 +0530

    Update ascending.sh

--- Exp 2/ascending.sh
+++ Exp 2/ascending.sh
@@ -5,7 +5,7 @@ n=${#arr[@]}  # Get the number of elements in the array
 
 for ((i = 0; i < n - 1; i++)); do
     for ((j = 0; j < n - i - 1; j++)); do
-        if [ ${arr[j]} -lt ${arr[j+1]} ]; then 
+        if [ ${arr[j]} -gt ${arr[j+1]} ]; then 
             temp=${arr[j]}
             arr[j]=${arr[j+1]}
             arr[j+1]=$temp
