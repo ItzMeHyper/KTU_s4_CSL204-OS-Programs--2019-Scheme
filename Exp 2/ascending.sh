35e4af6e9dcfe5198fb8dc93da5fad9f2bf7d35c
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2025-04-09 21:36:10 +0530

    Update ascending.sh

--- Exp 2/ascending.sh
+++ Exp 2/ascending.sh
@@ -3,11 +3,9 @@ read -a arr  # Read input as an array
 
 n=${#arr[@]}  # Get the number of elements in the array
 
-# Bubble Sort Algorithm (Descending Order)
 for ((i = 0; i < n - 1; i++)); do
     for ((j = 0; j < n - i - 1; j++)); do
-        if [ ${arr[j]} -lt ${arr[j+1]} ]; then  # Change '>' to '<' for descending order
-            # Swap elements
+        if [ ${arr[j]} -lt ${arr[j+1]} ]; then 
             temp=${arr[j]}
             arr[j]=${arr[j+1]}
             arr[j+1]=$temp
@@ -15,5 +13,4 @@ for ((i = 0; i < n - 1; i++)); do
     done
 done
 
-# Display sorted numbers
 echo "Sorted numbers in descending order: ${arr[@]}"
